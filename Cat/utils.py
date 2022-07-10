86239943bcf59800027227c00f3f31bf46995cac
mrtlckn <ahmet.telceken@hotmail.com>
2022-07-10 17:31:54 +0300

    boxes,classes,scores from detectron2.structures

--- Cat/utils.py
+++ Cat/utils.py
@@ -19,7 +19,6 @@ from detectron2.config import get_cfg
 from detectron2 import model_zoo
 
 from detectron2.utils.visualizer import ColorMode
-
 #libraries
 import random
 import cv2
@@ -64,8 +63,9 @@ def get_train_cfg(config_file_path, checkpoint_url, train_dataset_name, test_dat
     cfg.OUTPUT_DIR = output_dir
 
     return cfg
-
+from detectron2.structures import Instances
 from detectron2.data.datasets import register_coco_instances
+obj = Instances(image_size=(480, 640))
 def on_image(image_path,predictor):
 
     for d in ["train", "test"]:
@@ -74,12 +74,59 @@ def on_image(image_path,predictor):
     dataset_custom_metadata = MetadataCatalog.get("LP_train")
     im = cv2.imread(image_path)
     outputs = predictor(im)
-    v = Visualizer(im[:,:,::-1], metadata=dataset_custom_metadata,scale = 0.5,instance_mode=ColorMode.SEGMENTATION)
+    
+    v = Visualizer(im[:,:,::-1], metadata=dataset_custom_metadata,scale = 0.5,instance_mode=ColorMode.SEGMENTATION)#dataset_custom_metadata
     v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+    #print("Outputs:", outputs) #outputs["instances"] ile aynı 
+    print("All informations:",outputs["instances"])
+    print("Object 1:",outputs["instances"].pred_boxes[0][0])
+    print("Object 1, x:",outputs["instances"].pred_boxes[0][0].tensor.cpu().numpy()[0][0])
+    print("Object 1, y:",outputs["instances"].pred_boxes[0][0].tensor.cpu().numpy()[0][1])
+    print("Object 1, w:",outputs["instances"].pred_boxes[0][0].tensor.cpu().numpy()[0][2])
+    print("Object 1, h:",outputs["instances"].pred_boxes[0][0].tensor.cpu().numpy()[0][3])
 
+    #Sonradan yapılan Sql icin
+    idxofClass = [i for i, x in enumerate(list(outputs['instances'].pred_classes)) if x == 0]
+    o = outputs["instances"]
+    classes = o.pred_classes[idxofClass]
+    scores = o.scores[idxofClass]
+    boxes = o.pred_boxes[idxofClass]
+    masks = o.pred_masks[idxofClass]
+    print("idxofClass:", idxofClass)
+
+    print("classes : ", classes)
+    print("Classes but only one object: ", o.pred_classes)
+    print("classes lenght", len(classes))
+    print("Scores : ", scores[0])
+    print("boxes", boxes)
+    #Classes
+    print("##########CLASS##########")
+    classss = outputs["instances"].pred_classes.to("cpu").numpy()
+    print ("Classs: ", classss)
+    #Score
+    print("##########Score##########")
+    score = outputs["instances"].scores.to("cpu").numpy()
+    print("Score: : ",score)
+    print("Score only one :", score[0])
+    #print("Calisti mi : ", classes, "Scores: ",scores)
+
+    #obj icine atmak
+    #obj.set('pred_classes', classes)
+    #obj.set('scores', scores)
+    #obj.set('pred_boxes', boxes)
+    #obj.set('pred_masks', masks)
+    
+    
+   
+
+    #pred_classes
+    #print("pred_classes :",outputs["instances"].pred_classes[0][0].tensor.cpu().numpy()[0][0])
+    #print("Object 1:",outputs["instances"].num_instances[0])
+    
     plt.figure(figsize=(14,10))
     plt.imshow(v.get_image())
     plt.show()
+    
 
 #def on video():
 
